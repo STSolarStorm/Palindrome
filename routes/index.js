6ae626d24e158f168e73d976f2734363daf2f143
dgmp <dgmp@fresnou.org>
2023-09-06 09:27:05 -0700

    Working on Your Turn 1

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index',{
-    phrase: "taco Cat"
+    phrase: "taco Cat",
+    phraseEx: "Step On No Pets"
   });
 });
 
 router.post('/', function(req, res){
   res.render('index',{
     phrase: req.body.userText,
-    message: getResultDescription(req.body.userText)
+    message: getResultDescription(req.body.userText),
+    phraseEx: req.body.exText,
+    message2: getResultDescriptionEx(req.body.exText)
   })
 })
 
 function checkPalindrome(phrase){
-  let temp = phrase.split("");
+  let temp = phrase.trim();
+  temp = temp.split();
   temp = temp.reverse();
   temp = temp.join("")
-  phrase.replace(".", ",", ":", ";", "?", "'")
   if (phrase.toLowerCase().replace("","") === temp.toLowerCase().replace("","")){
     return true
   }
@@ -28,11 +32,33 @@ function checkPalindrome(phrase){
   }
 }
 
+function checkPalindromeEx(phraseEx){
+  let tempEx = phraseEx.trim();
+  tempEx = tempEx.split();
+  tempEx = tempEx.reverse();
+  tempEx = tempEx.join("")
+  if (phraseEx.toLowerCase().replace("","") === tempEx.toLowerCase().replace("","")){
+    return true
+  }
+  else {
+    return false
+  }
+}
+
+
+
 function getResultDescription(phrase) {
   if (checkPalindrome(phrase)) {
     return `${phrase} is a palindrome`
   }
   return `${phrase} is not a palindrome`
+}
+
+function getResultDescriptionEx(phraseEx) {
+  if (checkPalindromeEx(phraseEx)) {
+    return `${phraseEx} is a palindrome`
+  }
+  return `${phraseEx} is not a palindrome`
 }
 
 module.exports = router;
